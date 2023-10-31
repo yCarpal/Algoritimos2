c526ee7841bad78ba872d8b442e491a39cda0a6b
yCarpal <141735210+yCarpal@users.noreply.github.com>
2023-10-31 14:31:27 +0000

    massa

---
+++ manipulacao/arquivos/ex.1.py
@@ -0,0 +1,9 @@
+usuarios = open("minupulacao/arquivos/usuarios.txt","w") 
+quntUsu = 6
+espacoutilizado = []
+porcentagem = []
+capacidadeTotal = []
+espacomedio = []
+
+for i in range(usuarios):
+    usuarios.append("Insira um nome no arquivo")
