e59d99051e2fd9fdc20a813d076762c3adeefad7
yCarpal <141735210+yCarpal@users.noreply.github.com>
2023-11-09 01:10:53 +0000

    atttt

--- trabalgo/aqua.py
+++ trabalgo/aqua.py
@@ -1,4 +1,15 @@
-aqua = open('trabalgo/aquaviarios.csv', 'r')
-for linha in aqua:
-    colunas = linha.split(',')
-    print((colunas[3]))
+def filtrar_distritos(aqua):
+    aqua = open('trabalgo/aquaviarios.csv', 'r')  # Abra o arquivo 'trabalho/aquaviarios.csv'
+
+    with open(aqua, 'r') as trabalgo:
+        dados = trabalgo.readlines()
+
+    # Crie um filtro para obter apenas os distritos de 1 a 9
+    distritos_filtrados = [distrito.strip() for distrito in dados if 1 <= int(distrito.split(',')[0]) <= 9]
+
+    # Itere sobre os distritos filtrados e imprima seus respectivos órgãos
+    for distrito in distritos_filtrados:
+        codigo, orgao = distrito.split(',')
+        print(f"Distrito {codigo}: {orgao}")
+
+filtrar_distritos('aquaviario.txt')
